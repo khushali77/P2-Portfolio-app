192a357b56b3ffff675ab58fe508ed186ac63264
khushali77 <201801264@daiict.ac.in>
2020-12-05 17:33:01 +0530

    Removed clg name and added connection links

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -4,8 +4,14 @@ import {data} from "../profileData";
 function Profile(){
     return(
         <div className="profileinfo">
-        <h1>{data.Name},{data.college}</h1>
+        <h1>{data.Name}</h1>
         <p>{data.desc}</p>
+        <div className="connect">
+            <a href="https://www.instagram.com/khushaliiishahh/" target="_blank" rel="noreferrer"><img src={data.connect[0]} alt="connect"/></a>
+            <a href="https://www.linkedin.com/in/khushalishah7/" target="_blank" rel="noreferrer"><img src={data.connect[1]} alt="connect"/></a>
+            <a href="https://twitter.com/khushaliiishahh" target="_blank" rel="noreferrer"><img src={data.connect[2]} alt="connect"/></a>
+            <a href="https://github.com/khushali77" target="_blank" rel="noreferrer"><img src={data.connect[3]} alt="connect"/></a>
+        </div>
         </div>
     );
 }
